a37d31dd482a9485c93a71dc97b8be237bc8797d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/UseScaleBreaks/Form1.Designer.vb
@@ -0,0 +1,119 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace UseScaleBreaks
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+			Dim series1 As New DevExpress.XtraCharts.Series()
+			Dim sideBySideBarSeriesLabel1 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+			Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("A", New Object() { (CObj(5))})
+			Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("B", New Object() { (CObj(104))})
+			Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("C", New Object() { (CObj(2006))})
+			Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("D", New Object() { (CObj(3))})
+			Dim seriesPoint5 As New DevExpress.XtraCharts.SeriesPoint("E", New Object() { (CObj(108))})
+			Dim seriesPoint6 As New DevExpress.XtraCharts.SeriesPoint("F", New Object() { (CObj(2007))})
+			Dim seriesPoint7 As New DevExpress.XtraCharts.SeriesPoint("G", New Object() { (CObj(4))})
+			Dim seriesPoint8 As New DevExpress.XtraCharts.SeriesPoint("H", New Object() { (CObj(105))})
+			Dim seriesPoint9 As New DevExpress.XtraCharts.SeriesPoint("I", New Object() { (CObj(2009))})
+			Dim sideBySideBarSeriesLabel2 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
+			Me.checkEdit2 = New DevExpress.XtraEditors.CheckEdit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(sideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.checkEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' chartControl1
+			' 
+			Me.chartControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			xyDiagram1.AxisX.Label.Visible = False
+			xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+			xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+			Me.chartControl1.Diagram = xyDiagram1
+			Me.chartControl1.Legend.Visible = False
+			Me.chartControl1.Location = New System.Drawing.Point(12, 37)
+			Me.chartControl1.Name = "chartControl1"
+			sideBySideBarSeriesLabel1.LineVisible = True
+			series1.Label = sideBySideBarSeriesLabel1
+			series1.Name = "Series 1"
+			series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4, seriesPoint5, seriesPoint6, seriesPoint7, seriesPoint8, seriesPoint9})
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+			sideBySideBarSeriesLabel2.LineVisible = True
+			Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel2
+			Me.chartControl1.Size = New System.Drawing.Size(319, 278)
+			Me.chartControl1.TabIndex = 0
+			' 
+			' checkEdit1
+			' 
+			Me.checkEdit1.Location = New System.Drawing.Point(12, 12)
+			Me.checkEdit1.Name = "checkEdit1"
+			Me.checkEdit1.Properties.Caption = "Show Auto Scale Breaks"
+			Me.checkEdit1.Size = New System.Drawing.Size(157, 19)
+			Me.checkEdit1.TabIndex = 1
+'			Me.checkEdit1.CheckedChanged += New System.EventHandler(Me.checkEdit1_CheckedChanged);
+			' 
+			' checkEdit2
+			' 
+			Me.checkEdit2.Location = New System.Drawing.Point(175, 12)
+			Me.checkEdit2.Name = "checkEdit2"
+			Me.checkEdit2.Properties.Caption = "Show Manual Scale Breaks"
+			Me.checkEdit2.Size = New System.Drawing.Size(156, 19)
+			Me.checkEdit2.TabIndex = 2
+'			Me.checkEdit2.CheckedChanged += New System.EventHandler(Me.checkEdit2_CheckedChanged);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(343, 327)
+			Me.Controls.Add(Me.checkEdit2)
+			Me.Controls.Add(Me.checkEdit1)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(sideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.checkEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private chartControl1 As DevExpress.XtraCharts.ChartControl
+		Private WithEvents checkEdit1 As DevExpress.XtraEditors.CheckEdit
+		Private WithEvents checkEdit2 As DevExpress.XtraEditors.CheckEdit
+	End Class
+End Namespace
+
---
+++ VB/UseScaleBreaks/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("UseScaleBreaks")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UseScaleBreaks")>
+<Assembly: AssemblyCopyright("Copyright ©  2009")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("08ed2edb-e5d3-47d8-bdb0-5b6c66be0aab")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
