772a5de486e85923125155246482d260fffaab18
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to insert a scale break into an axis
+
+
+<p>This example demonstrates how both automatic and manual <a href="http://www.devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument6232.htm">scale breaks</a> can be created within an <a href="http://www.devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument6016.htm">axis</a>, and customized at runtime.</p><p>Note that both auto-created and manual scale breaks on an axis can co-exist, without affecting the chart's output, in cases where their ranges interfere.</p><p>See also: <a href="https://www.devexpress.com/Support/Center/p/E1621">How to enable automatic scale breaks for an axis</a>.</p>
+
+
+<h3>Description</h3>
+
+<p>Use check boxes, to enable either auto, or manual scale breaks, or both. Note that scale breaks are calculated, to allow the ranges of manual and auto scale breaks to overlap.</p>
+
+<br/>
+
+
---
+++ VB/UseScaleBreaks/Form1.vb
@@ -0,0 +1,53 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace UseScaleBreaks
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub checkEdit1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles checkEdit1.CheckedChanged
+			If checkEdit1.Checked Then
+				' Enable auto scale breaks and define their allowed number.
+				CType(chartControl1.Diagram, XYDiagram).AxisY.AutoScaleBreaks.Enabled = True
+				CType(chartControl1.Diagram, XYDiagram).AxisY.AutoScaleBreaks.MaxCount = 2
+
+				setScaleBreaksAppearance()
+			Else
+				CType(chartControl1.Diagram, XYDiagram).AxisY.AutoScaleBreaks.Enabled = False
+			End If
+		End Sub
+
+		Private Sub checkEdit2_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles checkEdit2.CheckedChanged
+			If checkEdit2.Checked Then
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreaks.Clear()
+
+				' Create scale breaks  manually, 
+				' with their Edge1 and Edge2 properties defined in the constructor.
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreaks.Add(New ScaleBreak("Scale Break 1", 10, 100))
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreaks.Add(New ScaleBreak("Scale Break 2", 110, 2000))
+
+				setScaleBreaksAppearance()
+			Else
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreaks.Clear()
+			End If
+		End Sub
+
+		Private Sub setScaleBreaksAppearance()
+			If (CType(chartControl1.Diagram, XYDiagram)).AxisY.ScaleBreaks.Count > 0 OrElse (CType(chartControl1.Diagram, XYDiagram)).AxisY.AutoScaleBreaks.Enabled Then
+
+				' Define the scale breaks' options.
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreakOptions.Style = ScaleBreakStyle.Waved
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreakOptions.SizeInPixels = 20
+				CType(chartControl1.Diagram, XYDiagram).AxisY.ScaleBreakOptions.Color = Color.RoyalBlue
+			End If
+		End Sub
+
+	End Class
+End Namespace
